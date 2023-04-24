e488b8def367197f822346e7dc3c7b65ed27aa66
Pedro Alejandro Pacheco Tripi <32602861+pedroalejandropt@users.noreply.github.com>
2023-04-24 20:55:50 +0200

    Update Handler, Fix Indentation && Uncomment Code

--- LambdaTriggerByS3Change/Handler.cs
+++ LambdaTriggerByS3Change/Handler.cs
@@ -20,61 +20,58 @@ namespace LambdaTriggerByS3Change
 		public Handler(IAmazonS3 amazonS3)
 		{
 			this.amazonS3 = amazonS3;
-			//this.elasticClient = elasticClient;
+			this.elasticClient = elasticClient;
 		}
 
 		public void Dispose() { }
 
 		public async Task Run(S3Event s3Event)
 		{
-            var s3Object = s3Event.Records?.FirstOrDefault().S3;
+            		var s3Object = s3Event.Records?.FirstOrDefault().S3;
 			Console.WriteLine($"RECORD: {JsonSerializer.Serialize(s3Event)}");
-            try
-            {
-                if (s3Object == null)
-                {
-                    return;
-                }
+			try
+			{
+			
+				if (s3Object == null) return;
+			
+				using (var client = new AmazonS3Client())
+				{
 
-                using (var client = new AmazonS3Client())
-                {
+			    		if (s3Event.Records?.FirstOrDefault().EventName == "ObjectRemoved:Delete")
+			    		{
+						Console.WriteLine($"FILE DELETED");
+						// UPDATE OPENSEARCH
+						await UpdateOpenSearch(fatherClass, openSearchJson);
+			    		}
+			    		else
+			    		{
+						var file = await client.GetObjectAsync(s3Object.Bucket.Name, s3Object.Object.Key);
+						using var reader = new StreamReader(file.ResponseStream);
+						var fileContents = await reader.ReadToEndAsync();
 
-                    if (s3Event.Records?.FirstOrDefault().EventName == "ObjectRemoved:Delete")
-                    {
-                        Console.WriteLine($"FILE DELETED");
-                        // UPDATE OPENSEARCH
-                        // await UpdateOpenSearch(fatherClass, openSearchJson);
+						var fatherClass = JsonConvert.DeserializeObject<FatherClass>(fileContents);
+						var openSearchJson = JsonSerializer.Serialize(fatherClass, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+						Console.WriteLine($"OpenSearch Object: {JsonSerializer.Serialize(openSearchJson)}");
 
-                    }
-                    else
-                    {
-                        var file = await client.GetObjectAsync(s3Object.Bucket.Name, s3Object.Object.Key);
-                        using var reader = new StreamReader(file.ResponseStream);
-                        var fileContents = await reader.ReadToEndAsync();
+						// UPDATE OPENSEARCH
+						await UpdateOpenSearch(fatherClass, openSearchJson);
+					}
+				}
+			}
+		    	catch (Exception e)
+		    	{
+				Console.WriteLine($"ERROR: {e.Message}, \nNEW IMAGE DATA: {JsonSerializer.Serialize(s3Object)}");
+				throw;
+		    	}
+		}
 
-                        var fatherClass = JsonConvert.DeserializeObject<FatherClass>(fileContents);
-                        var openSearchJson = JsonSerializer.Serialize(fatherClass, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-                        Console.WriteLine($"OpenSearch Object: {JsonSerializer.Serialize(openSearchJson)}");
-
-                        // UPDATE OPENSEARCH
-                        // await UpdateOpenSearch(fatherClass, openSearchJson);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"ERROR: {e.Message}, \nNEW IMAGE DATA: {JsonSerializer.Serialize(s3Object)}");
-                throw;
-            }
-        }
-
-        public async Task UpdateOpenSearch(FatherClass fatherClass, string openSearchJson)
-        {
-            await this.elasticClient.LowLevel.IndexAsync<BytesResponse>(
-                "index-name",
-                fatherClass.Id,
-                PostData.String(openSearchJson)
-            );
-        }
-    }
+        	public async Task UpdateOpenSearch(FatherClass fatherClass, string openSearchJson)
+        	{
+            		await this.elasticClient.LowLevel.IndexAsync<BytesResponse>(
+                		"index-name",
+                		fatherClass.Id,
+                		PostData.String(openSearchJson)
+            		);
+        	}
+    	}
 }
